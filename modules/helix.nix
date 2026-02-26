bf477a142be90adfa55de59fe8b9b96b1b5ee925
T-Kisaku <kisakutakashi0226@gmail.com>
2026-02-26 22:13:29 +0900

    refactor(zellij): move lazygit, yazi shortcut to zellij
    helix shouldn't access to zellij pane

--- modules/helix.nix
+++ modules/helix.nix
@@ -10,32 +10,6 @@
         clipboard-provider = "termcode";
         auto-save.after-delay.enable = true;
       };
-      keys.normal = {
-        space.g = [
-          ":write-all"
-          ":new"
-          ":sh zellij run -n Lazygit -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- lazygit"
-          ":set mouse false"
-          ":set mouse true"
-          ":buffer-close!"
-          ":redraw"
-          ":reload-all"
-        ];
-        space.space = [
-          ":write-all"
-          ":new"
-          ":sh zellij run -n Yazi -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- yazi"
-          ":set mouse false"
-          ":set mouse true"
-          ":buffer-close!"
-          ":redraw"
-          ":reload-all"
-        ];
-        esc = [
-          "collapse_selection"
-          "keep_primary_selection"
-        ];
-      };
       theme = "tokyonight_storm";
     };
 
